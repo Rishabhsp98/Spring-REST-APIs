c34a972c86042dbe071cdfab8085742c9996b927
Rishabh Sharma <rish6501@gmail.com>
2023-06-05 23:40:07 +0530

    Added Put, Patch, Delete APIs to the Controller

--- src/main/java/com/example/springrestapis/springcontrollerAPIs.java
+++ src/main/java/com/example/springrestapis/springcontrollerAPIs.java
@@ -20,7 +20,11 @@ public class springcontrollerAPIs {
     }
 
     @GetMapping("/employee/id/{id}")
-    public restApimodel getEmployee(@PathVariable("id") int id){
+    public restApimodel getEmployee(@PathVariable("id") int id) throws Exception {
+        if(!mp.containsKey(id))
+        {
+            throw new Exception("User Does not Exist");
+        }
         return this.mp.get(id);
     }
 
@@ -31,5 +35,44 @@ public class springcontrollerAPIs {
             throw new Exception("Employee Id can not be null!");
         }
         this.mp.put(restApimodel.getId(),restApimodel); // storing map of request body to id
+    }
+
+    @PatchMapping("/employee/id/{id}")
+    public restApimodel patchUpdateEmployee(@RequestBody restApimodel restApimodel){
+
+        restApimodel employeeFromMap = this.mp.get(restApimodel.getId());
+
+        if(restApimodel.getAge() != null){
+            employeeFromMap.setAge(restApimodel.getAge());
+        }
+
+        if(restApimodel.getName() != null){
+            employeeFromMap.setName(restApimodel.getName());
+        }
+
+        return this.mp.put(restApimodel.getId(), employeeFromMap);
+    }
+
+
+    @PutMapping("/employee/id/{id}")
+    public void updateEmployee(@RequestBody restApimodel restApimodel) throws Exception {
+        if(restApimodel.getId() == null || !this.mp.containsKey(restApimodel.getId()))
+        {
+            throw new Exception("Not valid request!!");
+        }
+        this.mp.put(restApimodel.getId(),restApimodel); // storing map of request body to id
+    }
+
+    // put api , patch api and delete api
+
+
+
+    @DeleteMapping("/employee/id/{id}")
+    public void deleteEmployee(@PathVariable int id) throws Exception {
+        if(!mp.containsKey(id))
+        {
+            throw new Exception("User Does not Exist");
+        }
+        this.mp.remove(id);
     }
 }
